678aa029fb8a881aadc78fb87aa2689343bfe78d
MyronFanQiu <fanqiu@microsoft.com>
2020-09-21 16:37:52 +0800

    regen azure_mgmt_digital_twins

--- management/azure_mgmt_digital_twins/lib/azure_mgmt_digital_twins.rb
+++ management/azure_mgmt_digital_twins/lib/azure_mgmt_digital_twins.rb
@@ -2,6 +2,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-module Azure
-  VERSION = '0.63.1'
-end
+require '2020-10-31/generated/azure_mgmt_digital_twins'
+require 'profiles/latest/digitaltwins_latest_profile_client'
--- management/azure_mgmt_digital_twins/lib/module_definition.rb
+++ management/azure_mgmt_digital_twins/lib/module_definition.rb
@@ -2,6 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-module Azure
-  VERSION = '0.63.1'
-end
+module Azure end
+module Azure::DigitalTwins end
+module Azure::DigitalTwins::Mgmt end
